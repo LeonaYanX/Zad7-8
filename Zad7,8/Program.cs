a514fe48e9f842da159b3d618ddb9777f962c15d
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-04-04 19:57:44 +0400

    Add project files.

---
+++ Zad7,8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad7_8
+{
+    /* Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+     * и возвращающий новый массив, каждый элемент которого равен сумме элементов
+     * двух входящих массивов в той же ячейке.
+     * Если длины массивов не равны, необходимо как-то оповестить пользователя.*/
+    internal class Program
+    {
+        public static void PrintArray(int[] array)
+        {
+        for (int i = 0; i < array.Length; i++) 
+            {
+                Console.Write(array[i] + "\t");
+            }
+        }
+        public static int[] GetSummedArray(int[] array1 , int[] array2) 
+        {
+            if (array1 == null || array2 == null) 
+            {
+                throw new ArgumentNullException("One of the reference typed arguments is null");
+                
+            } 
+               
+            if (array1.Length==array2.Length)
+            {
+                int[] summedArray = new int[array2.Length];
+                for (int i = 0; i < summedArray.Length; i++)
+                {
+                    summedArray[i] = array1[i]+array2[i];
+                }
+                return summedArray;
+            }
+            else 
+            {
+                throw new InvalidOperationException("Array lengths are not simular");
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray1 = {1,2,3,4};
+            int[] myArray2 = {5,6,7,8};
+            int[] resultArray = GetSummedArray(myArray1,myArray2);
+            PrintArray(resultArray);
+            
+        }
+    }
+}
---
+++ zad8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad8
+{
+    /*Реализуйте метод, принимающий в качестве аргументов двумерный массив. 
+     * Метод должен проверить что длина строк и столбцов с одинаковым индексом одинакова,
+     * детализировать какие строки со столбцами не требуется. 
+     * Как бы вы реализовали подобный метод?*/
+    internal class Program
+    {
+        public static bool IsSimularRowColumn(int[][] array)
+        {
+            bool result = true;
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i].Length != array.Length) 
+                {
+                result = false;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[][] myArray = new int[][]
+            { 
+            new int [] {1,2},
+            new int [] {2,3,4},
+            new int [] {3,7}
+            };
+            bool isSimular = IsSimularRowColumn(myArray);
+            Console.WriteLine($"Is Row and Column count simular ? :{isSimular} ");
+
+        }
+    }
+}
